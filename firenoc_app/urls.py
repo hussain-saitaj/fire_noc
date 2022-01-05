b1ee73b2b687ee9d4d770c1021f6fb480e94445c
Hussain <taj4357@gmail.com>
2022-01-05 11:19:18 +0530

    second commit

--- firenoc_app/urls.py
+++ firenoc_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('',views.login,name='login'),
-    path('register',views.register,name="register")
+    
+    path('login',views.login,name='login'),
+    path('register',views.register,name="register"),
+    path('general',views.general,name="general"),
+    path('upload',views.upload,name="file_upload"),
+    path('download',views.download,name="download"),
 ]
--- firenoc_app/views.py
+++ firenoc_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User,auth
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.http.response import HttpResponse
+import os
+import mimetypes
 
 # Create your views here.
 
@@ -21,3 +26,44 @@ def login(request):
 
 def register(request):
     return render(request,'register.html')
+
+def general(request):
+    if request.method=='POST':
+        return render(request,'form.html')
+    
+    return render(request,'general.html')
+
+def upload(request):
+    if request.method == 'POST' and 'docfile' in request.FILES:
+        print("hai")
+        myfile = request.FILES
+        file=myfile['docfile']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'appFee.html', {
+            'uploaded_file_url': filename
+        })
+    print("hello")    
+    return render(request, 'form.html')
+
+
+
+def download(request):
+    # Define Django project base directory
+    #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = 'front.pdf'
+    # Define the full file path
+    value=os.path.join(str(settings.BASE_DIR), "templates/")
+    filepath =  value+ filename
+    # Open the file for reading content
+    path = open(filepath, 'rb')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
